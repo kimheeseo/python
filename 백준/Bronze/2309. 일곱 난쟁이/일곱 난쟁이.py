b0aa6b6b2eef5414c08b9d14b2df3cadac62e069
kimheeseo <43293606+kimheeseo@users.noreply.github.com>
2024-11-11 18:24:31 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -7,7 +7,6 @@ for _ in range(9):
     arr.append(f)
 arr.sort()
 
-
 sum_ = sum(arr)
 fake=[]
 # 7번 반복 대신 반대로 생각 9명 합에서 2명을 뺐을때 100 이하인 경우 찾기
@@ -18,7 +17,6 @@ for i in range(9):
         if sum_-arr[i]-arr[j] ==100:
             fake.append(arr[i])
             fake.append(arr[j])
-            #list del method은 코테 풀이할때 가급적 쓰지 말자!
 
 for i in arr:
     if i in fake:

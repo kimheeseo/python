bb620f23217e979c0d2818cc97499fea3915e147
kimheeseo <43293606+kimheeseo@users.noreply.github.com>
2024-11-12 08:55:05 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,11 +1,14 @@
+import sys
+input=sys.stdin.readline
+
 a=int(input())
 q=[]
 
 for i in range(a):
-  q=[]
-  b=int(input())
-  for i in [25, 10, 5, 1]:
-    q.append(b//int(i))
-    b=b%int(i)
-  for i in q:
-    print(i, end=" ")
+    q=[]
+    b=int(input())
+    for i in [25, 10, 5, 1]:
+        q.append(b//int(i))
+        b=b%int(i)
+    for i in q:
+        print(i, end=" ")

aa6fab8bf069a500710d10d518e2a7c1eee06e24
kimheeseo <43293606+kimheeseo@users.noreply.github.com>
2024-11-14 11:32:06 +0900

    [Bronze II] Title: 소수 찾기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -3,19 +3,12 @@ cnt=0
 
 b=list(map(int,input().split()))
 
-for i in range(len(b)):
- # print('b[i]값:',b[i])
-  if b[i]==1:
-    cnt=cnt+1
- #   print('1입니다.')
-  for j in range(2, b[i]):  
-#    print('j값:',j)
-
-    if b[i]%j ==0:
-     # print('소수인가?')
-   # else:
-     # print('소수 아닙니다.')
-      cnt=cnt+1
-      break
-
-print(a-cnt)  
+for i in range(a):
+    if b[i] ==1:
+        cnt=cnt+1
+    else:
+        for j in range(2,int(b[i]**0.5)+1):
+            if b[i]%j==0:
+                cnt=cnt+1
+                break
+print(len(b)-cnt)

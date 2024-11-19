a3e4a8475bf27619d9be5c0ea9e6f6deae2f8b22
kimheeseo <43293606+kimheeseo@users.noreply.github.com>
2024-11-20 08:47:54 +0900

    [Bronze V] Title: 火曜日 (Tuesday), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/31611. 火曜日 （Tuesday）/火曜日 （Tuesday）.py
@@ -0,0 +1,13 @@
+import sys
+input=sys.stdin.readline
+
+def sol(x):
+    if x % 7 == 2:
+        return 1
+    else:
+        return 0
+ 
+ 
+if __name__ == '__main__':
+    x = int(input())
+    print(sol(x))

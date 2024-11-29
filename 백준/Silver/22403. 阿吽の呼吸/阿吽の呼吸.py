e78926144c51fcea3ffc299655651240a9c5d4a6
kimheeseo <43293606+kimheeseo@users.noreply.github.com>
2024-11-29 15:15:08 +0900

    [Silver V] Title: 阿吽の呼吸, Time: 56 ms, Memory: 34000 KB -BaekjoonHub

--- 백준/Silver/22403. 阿吽の呼吸/阿吽の呼吸.py
+++ 백준/Silver/22403. 阿吽の呼吸/阿吽の呼吸.py
@@ -1,7 +1,3 @@
-# 해당 문제는 마에다(A)와 고토(B)가 순서대로 말을 한다는 것이다.
-# 예를 들어, AABB, ABAB는 규칙적이지만, ABBA와 같이 둘이 말한 횟수는 같지만,
-# 규칙적이지 않은 것이니, deque으로 넣었다가 뺏다가 하면 된다.
-
 from collections import deque
 
 a = int(input())
@@ -10,10 +6,10 @@ error = 0
 
 for _ in range(a):
     c = input()
-    if c == 'A':
+    if c == 'A': 
         b.append(c)
     else:
-        if not b:
+        if not b:  
             error = 1
             break
         b.popleft()

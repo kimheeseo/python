f4359771402c80968e1c87028d62693eb23d8d85
kimheeseo <43293606+kimheeseo@users.noreply.github.com>
2024-12-02 10:58:44 +0900

    [Bronze I] Title: 콘도 선정, Time: 300 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Bronze/2246. 콘도 선정/콘도 선정.py
+++ 백준/Bronze/2246. 콘도 선정/콘도 선정.py
@@ -11,3 +11,6 @@ for i in condos:
         cost = temp
         result += 1
 print(result)
+
+# 해당 문제 풀이법은 우선 바닷가에서 가까운 것을 찾기위해서 sort를 한 다음에
+# 가격이 저렴한 것을 찾아야하니까, if temp<cost:를 통해, 짧으면서 가격이 저렴한 것을 찾는다.
